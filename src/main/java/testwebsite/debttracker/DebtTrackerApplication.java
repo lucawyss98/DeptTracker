eb837e3520bc3f3ee8281ad33dd6320dfa860bff
lucawyss98 <luca.wyss98@hotmail.com>
2023-08-29 21:40:49 +0200

    Initial Push

---
+++ src/main/java/testwebsite/debttracker/DebtTrackerApplication.java
@@ -0,0 +1,13 @@
+package testwebsite.debttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebtTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/testwebsite/debttracker/DebtTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package testwebsite.debttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
